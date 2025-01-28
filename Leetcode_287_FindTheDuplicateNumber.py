1e9cc8193bb714394d6f6c41f225e83ab6b97dcf
KTbyKB <bashavlsi@gmail.com>
2025-01-28 15:42:22 +0530

    Add files via upload

---
+++ Leetcode_287_FindTheDuplicateNumber.py
@@ -0,0 +1,8 @@
+#287. Find the Duplicate Number
+def findDuplicate(self, nums: List[int]) -> int:
+    s =set()
+    for i in nums:
+        if i not in s:
+            s.add(i)
+        else:
+            return i
